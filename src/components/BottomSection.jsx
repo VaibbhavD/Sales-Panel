6fa84eaa9a35af7415b3c88da91968978ead03fa
Vaibhav D <vaibhavdhamanage12@gmail.com>
2024-09-24 11:55:09 +0530

    update bottom section make reduce sizes and update margin and padding

--- src/components/BottomSection.jsx
+++ src/components/BottomSection.jsx
@@ -5,17 +5,17 @@ const BottomSection = ({ invoice, setInvoice }) => {
   };
 
   return (
-    <div className="flex justify-between mb-8">
+    <div className="flex justify-between mt-2">
       {/* Left Side - Payment Type and Description */}
-      <div className="w-1/2 pr-6">
+      <div className="w-1/2">
         <label className="block text-gray-600 mb-2 text-sm font-medium">
-          Payment Type
+          Payment Type -
         </label>
         <select
           name="paymentType"
           value={invoice.paymentType}
           onChange={handleInputChange}
-          className="p-3 bg-gray-50 rounded-lg shadow-sm w-full mb-6 text-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          className="p-3 bg-gray-50 rounded-lg shadow-sm  mb-6 text-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-500"
         >
           <option value="Cash">Cash</option>
           <option value="Card">Card</option>
@@ -24,7 +24,7 @@ const BottomSection = ({ invoice, setInvoice }) => {
         </select>
 
         <label className="block text-gray-600 mb-2 text-sm font-medium">
-          Description
+          Description{"- "}
         </label>
         <textarea
           name="description"
